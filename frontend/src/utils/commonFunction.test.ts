39bcc36c9aa5f4dfa50a37458b86c6fc1c63f62e
Moon-gd <king4mun@gmail.com>
2023-04-27 14:20:43 +0900

    feat: #31 - install create commonFunction.test.ts

---
+++ frontend/src/utils/commonFunction.test.ts
@@ -0,0 +1,23 @@
+import { changeCSS } from "./commonFunction";
+
+describe("changeCSS()", () => {
+  test("set css value for proper inputs", () => {
+    const $target = document.createElement("div");
+    const property = "color";
+    const value = "rgb(255, 255, 255)";
+
+    changeCSS($target, property, value);
+
+    expect($target.style[property]).toBe(value);
+  });
+
+  test("don't set css value for improper inputs", () => {
+    const $target = document.createElement("div");
+    const property = "impropervalue";
+    const value = "rgb(255, 255, 255)";
+
+    changeCSS($target, property, value);
+
+    expect($target.style.getPropertyValue(property)).toBeFalsy();
+  });
+});
