cce3dbf209155ee51a38c9a8e4d5b465f311329a
Moon <moon@Moonui-MacBookPro.local>
2023-05-14 16:10:13 +0900

    add#103 - 상세 페이지 프로젝트 헤더 부분 제작 중

---
+++ frontend/src/ProjectDetail/ProjectDetailContent.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./projectDetailContent.scss";
+
+function ProjectDetailContent() {
+    return (
+        <section>projectDetailContent</section>
+    )
+}
+
+export default ProjectDetailContent;
