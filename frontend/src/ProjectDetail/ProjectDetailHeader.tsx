b684c7fc835f0ead32720e80098c11ae3833578c
Moon <moon@Moonui-MacBookPro.local>
2023-05-18 19:48:04 +0900

    add#103 - 커스텀 이미지 슬라이더 뷰 제작 및 이벤트 연동

--- frontend/src/ProjectDetail/ProjectDetailHeader.tsx
+++ frontend/src/ProjectDetail/ProjectDetailHeader.tsx
@@ -2,16 +2,14 @@ import React from 'react';
 import "./projectDetailHeader.scss";
 import ImageSlider from './ImageSlider';
 
-function ProjectDetailHeader() {
+function ProjectDetailHeader() { 
     return (
         <section className="projectDetailHeader">
             <section className="titleArea">
                 <h4 className="titleArea__category">캐릭터 굿즈</h4>
                 <h1 className="titleArea__title">울면 안 돼 근손실 나니까, 우리를 지켜줄 마동곰 키링</h1>
             </section>
-            <section className="imageArea">
-                image
-            </section>
+            <ImageSlider/>
             <section className="infoArea">info</section>
         </section>
     )
