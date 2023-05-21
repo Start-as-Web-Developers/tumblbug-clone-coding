20e0abb51e003a4baef8d924a671aa31530420d4
zangsu <zangsu_@naver.com>
2023-05-22 01:03:50 +0900

    feat : #100 - make login logic in UserController

--- backend/tumblbug-clone/src/main/java/com/example/tumblbugclone/managedconst/UserConst.java
+++ backend/tumblbug-clone/src/main/java/com/example/tumblbugclone/managedconst/UserConst.java
@@ -5,4 +5,8 @@ public class UserConst {
     //== USER SAVE CONST ==//
     public static final int DUPLICATED_ID = -1;
     public static final int DUPLICATED_EMAIL = -2;
+
+    //== USER EXCEPTION MESSAGE ==//
+    public static final String NO_USER_FOUNDED_MESSAGE = "No User Founded";
+    public static final String WRONG_PASSWORD = "Password is wrong";
 }
